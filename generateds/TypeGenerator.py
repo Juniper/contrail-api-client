4df75f46701eb4ed09917b133925e2579b66b712
Maciej Łastawiecki <maciejl46@gmail.com>
2019-02-04 17:01:53 +0100

    Add object to dictionary conversion
    Change-Id: I7b841219ff9d9a2030932825714f4c5042885d03
    Partial-Jira-Bug: JBE-871

--- generateds/TypeGenerator.py
+++ generateds/TypeGenerator.py
@@ -1754,13 +1754,16 @@ class PyGenerator(object):
     def generateExportDict(self, wrt, element):
         name = element.getName()
         base = element.getBase()
-        wrt("    def exportDict(self, name_='%s'):\n" % (name, ))
+        wrt("    def exportDict(self, name_='%s'):\n" % name)
         wrt('        # do obj->json->dict to handle nested complextype in object\n')
         wrt('        obj_json = json.dumps(self, default=lambda o: dict((k, v) for k, v in o.__dict__.iteritems()))\n')
         wrt('        obj_dict = json.loads(obj_json)\n')
         wrt('        if name_:\n')
         wrt('            return {name_: obj_dict}\n')
-        wrt('        return obj_dict\n')
+        wrt('        return obj_dict\n') 
+        wrt("    def to_dict(self, name_='%s'):\n" % name)
+        wrt('        # wrapper for exportDict to be consistent with resources from resource_client.py\n')
+        wrt('        return self.exportDict(name_)\n')
 
     def generateBuild(self, wrt, element):
         base = element.getBase()
