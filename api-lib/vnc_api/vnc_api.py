11cb0503d076497283751623782c7d49c3fba4fa
Mateusz Neumann <mateusz.neumann@codilime.com>
2019-02-27 12:13:44 +0100

    [WIP] Change how data is passed in get_int_owner
    VncApi passes GET requests data in query string, not message body.
    So we need not use JSON-encoded dict, but a simple dict instead.
    
    Change-Id: If1b2ab7da38646d5dfba7de4c4938c4fe6c8cd62
    Partial-jira-bug: JBE-959

--- api-lib/vnc_api/vnc_api.py
+++ api-lib/vnc_api/vnc_api.py
@@ -1292,10 +1292,13 @@ class VncApi(object):
 
     @check_homepage
     def get_int_owner(self, pool_name, index):
-        json_body = json.dumps({'pool': pool_name, 'value': index})
+        query_params = {'pool': pool_name, 'value': index}
         uri = self._action_uri['int-pool']
-        content = self._request_server(OP_GET, uri, data=json_body)
-        return json.loads(content)['owner']
+        content = self._request_server(OP_GET, uri, data=query_params)
+        try:
+            return content['owner']
+        except TypeError:
+            return json.loads(content)['owner']
     # end get_int_owner
 
     @check_homepage
